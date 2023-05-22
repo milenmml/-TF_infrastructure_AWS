6624a889aeec886c4ab30fe6dac6eef45fe2b837
milenmml <118394030+milenmml@users.noreply.github.com>
2023-05-22 07:27:29 +0300

    Create variables.tf for ec2 module

---
+++ ec2/variables.tf
@@ -0,0 +1,11 @@
+# --- ec2/variables
+variable "instance_count" {}
+variable "instance_type" {}
+variable "private_subnets" {}
+variable "vol_size" {}
+variable "lb_target_group_arn" {}
+variable "private_sg" {}
+variable "key_name" {}
+variable "public_key_path" {
+  
+}
